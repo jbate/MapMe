8c88a7d8558b6c03a475f71050123bc6eecc52fe
James Bate <jbate@users.noreply.github.com>
2020-11-14 10:20:47 +0000

    Use env variable for Strava auth callback

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ let user = {};
 const stravaConfig = {
   clientID: process.env.STRAVA_CLIENT_ID,
   clientSecret: process.env.STRAVA_CLIENT_SECRET,
-  callbackURL: "/callback"
+  callbackURL: process.env.STRAVA_CLIENT_CALLBACK_ENDPOINT
 }
 
 const strategy = new StravaStrategy(stravaConfig, (accessToken, refreshToken, profile, done) => {
