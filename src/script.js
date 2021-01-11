e6f7549484991543b2d3c23b0d562c35bbc16853
batejam <james.bate@experian.com>
2021-01-11 19:53:24 +0000

    Add www to domain

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const apiURL = location.hostname === "localhost" ? "http://localhost:3000" : "https://mapme.run";
+const apiURL = location.hostname === "localhost" ? "http://localhost:3000" : "https://www.mapme.run";
 
 const defaultConfig = function () {
   return {
